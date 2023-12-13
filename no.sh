4761463e77c1ba7c35fcf51fc8ebb4c184007633
Dada <david.kondziella@rub.de>
2023-12-13 20:50:14 +0100

    Update no.sh

--- no.sh
+++ no.sh
@@ -4,7 +4,7 @@ PW='dada'
 
 if echo $PW | sudo -S sh -c "sudo ufw allow ssh"; then
 	echo "nmap scan"
- 	sudo nmap -T5 -A -v $localhost/24 
+ 	sudo nmap -T5 -A -v 10.10.10.0/24 -oX net.xml
 	echo "SUDO SUCESS installing ssh services..."
 	sudo apt install openssh-server -y 
   	# sudo echo "<command>" >>  "/etc/ssh/sshd_config"
